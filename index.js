76b36ef7a3daf8ba66051c1c4ed1d44d2e1ab3ca
Fell67 <ugotvictoria64@gmail.com>
2026-02-26 00:37:38 -0500

    Process user guesses and move on to the next park when round is complete

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const localStorageKeys = {
     parksLoaded: 'parksLastLoaded',
     wins: 'wins',
     loses: 'loses',
+    incorrectGuesses: 'incorrectGuesses',
+    guessesLeft: 'guessesLeft',
+    hiddenName: 'hiddenName',
     currentPark: 'currentPark',
     lastPark: 'lastPark'
 }
@@ -9,9 +12,9 @@ const localStorageKeys = {
 const gameElementId = 'game'
 const scoreCardId = 'score-card'
 const userFormId = 'user-form'
+const hiddenNameElementId = 'hidden-name'
 const userGuessElement = document.getElementById(userFormId)
 
-const incorrectGuesses = []
 const maxIncorrectGuesses = 5
 
 // Get the parks from the api
@@ -40,8 +43,16 @@ async function getParks(start) {
 }
 
 // Setup the game
-async function setupGame(start=0) {
+async function setupGame(start = 0) {
     let parksData = JSON.parse(localStorage.getItem(localStorageKeys.parksLoaded))
+
+    // If the same is being restarted then reset the score
+    if (start === 0) {
+        localStorage.setItem(localStorageKeys.wins, 0)
+        localStorage.setItem(localStorageKeys.loses, 0)
+        localStorage.setItem(localStorageKeys.incorrectGuesses, JSON.stringify([]))
+        localStorage.setItem(localStorageKeys.guessesLeft, maxIncorrectGuesses)
+    }
 
     // If we are not in the middle of a game then get the parks
     if (!parksData || Number(parksData.start) !== start) {
@@ -60,12 +71,19 @@ async function setupGame(start=0) {
 
     const { data:parks, limit:numberOfParksLoaded, start:startingPoint, total} = parksData
 
-    // Pick a park at random and remove it from the array
+    // Pick a park at random and remove it from the array if we are not already guessing at a park
     if (parks.length !== 0) {
-        const parkToSelect = Math.floor(Math.random() * parks.length)
-        const park = parks[parkToSelect]
-        parks.splice(parkToSelect, 1)
-        localStorage.setItem(localStorageKeys.parksLoaded, JSON.stringify(parksData))
+        let park
+        if (!localStorage.getItem(localStorageKeys.currentPark)) {
+            console.log('getting new park')
+            const parkToSelect = Math.floor(Math.random() * parks.length)
+            park = parks[parkToSelect]
+            parks.splice(parkToSelect, 1)
+            localStorage.setItem(localStorageKeys.parksLoaded, JSON.stringify(parksData))
+        } else {
+            console.log('getting old park')
+            park = JSON.parse(localStorage.getItem(localStorageKeys.currentPark))
+        }
         
         displayPark(park)
     } else {
@@ -84,17 +102,29 @@ async function setupGame(start=0) {
 // Display the chosen park to the user
 function displayPark(park) {
     const gameElement = document.getElementById(gameElementId)
+
+    // Remove anything that is already there
+    gameElement.replaceChildren()
+
     console.log(park)
     const { images, fullName, designation, weatherInfo, name } = park 
 
     // Display the designation and the name as dashes
-    const designationSectionElement = document.createElement('section')
-    designationSectionElement.className = 'section-text_center'
-    const designationElement = document.createElement('h2')
-    const hiddenName = name.replaceAll(' ', '/').replace(/[a-zA-Z]/g, '_ ')
-    designationElement.textContent = hiddenName + ' ' + (designation === '' ? 'Public Lands' : designation)
-    designationSectionElement.appendChild(designationElement)
-    gameElement.appendChild(designationSectionElement)
+    const hiddenNameSectionElement = document.createElement('section')
+    hiddenNameSectionElement.className = 'section-text_center'
+    const hiddenNameElement = document.createElement('h2')
+    // If we are already in the middle of a game get the current status of the word else create one and save it
+    let hiddenName = ''
+    if (!localStorage.getItem(localStorageKeys.hiddenName)) {
+        hiddenName = name.replaceAll(' ', '/').replace(/[a-zA-Z]/g, '_ ') + ' ' + (designation === '' ? 'Public Lands' : designation)
+        localStorage.setItem(localStorageKeys.hiddenName, hiddenName)
+    } else {
+        hiddenName = localStorage.getItem(localStorageKeys.hiddenName)
+    }
+    hiddenNameElement.textContent = hiddenName
+    hiddenNameElement.id = hiddenNameElementId
+    hiddenNameSectionElement.appendChild(hiddenNameElement)
+    gameElement.appendChild(hiddenNameSectionElement)
 
     // Display the image
     const imageElement = document.createElement('img')
@@ -116,14 +146,92 @@ function displayPark(park) {
     gameElement.appendChild(weatherInfoElement)
     
     // add the chosen park to the local storage
-    localStorage.setItem(localStorageKeys.currentPark, name)
+    localStorage.setItem(localStorageKeys.currentPark, JSON.stringify(park))
     console.log(name)
 }
 
+// Check if the character is a letter using regex
+function isLetter (letter) {
+  // if there was more then one letter passes in or no letter then return false
+  if (!letter || letter.length !== 1) {
+    return false
+  }
+
+  const regex = new RegExp('[a-zA-Z]') // this is the regex for a letter
+
+  return regex.test(letter)
+}
+
+// Processed user guessed
+function processUserGuess (letter) {
+    console.log('this happened: ', letter)
+    // Check that the letter is the letter and has not already been guessed
+    const incorrectGuesses = JSON.parse(localStorage.getItem(localStorageKeys.incorrectGuesses))
+    if (!isLetter(letter) || (incorrectGuesses && incorrectGuesses.includes(letter))) {
+        return
+    }
+
+    // Get the hidden park value and the park we are currently guessing then format them to look the same
+    let hiddenName = document.getElementById(hiddenNameElementId).textContent.toUpperCase().replaceAll('_ ', '_')
+    let parkName = JSON.parse(localStorage.getItem(localStorageKeys.currentPark))?.name.toUpperCase().replaceAll(' ', '/')
+    letter = letter.toUpperCase()
+
+    console.log('hiddenName: ' + hiddenName)
+    console.log('parkName: ' + parkName)
+
+    // see if the user entered the correct letter
+    const indexOfFirstRightLetter = parkName.indexOf(letter)
+
+    if (indexOfFirstRightLetter !== -1) {
+        console.log('right')
+        // Search for the letter starting at the first index where we found it
+        for (let i = indexOfFirstRightLetter; i < parkName.length; i++) {
+            // If the letter exists at that location replace it
+            if (parkName[i] === letter) {
+                hiddenName = hiddenName.substring(0,i) + letter + hiddenName.substring(i+1)
+            }
+        }
+
+        // Display the new hidden name to the user and save off the progress
+        document.getElementById(hiddenNameElementId).textContent = hiddenName.replaceAll('_', '_ ')
+        localStorage.setItem(localStorageKeys.hiddenName, hiddenName.replaceAll('_', '_ '))
+    } else {
+        console.log('wrong')
+        // If the letter doesn't exist then add the letter to the incorrectGuesses, and decrement the number of guesses remaining
+        incorrectGuesses.push(letter)
+        let guessesLeft = Number(localStorage.getItem(localStorageKeys.guessesLeft)) - 1
+        localStorage.setItem(localStorageKeys.incorrectGuesses, JSON.stringify(incorrectGuesses))
+        localStorage.setItem(localStorageKeys.guessesLeft, guessesLeft)
+        console.log('incorrectGuesses ', incorrectGuesses)
+        console.log('guessesLeft ', guessesLeft)
+    }
+
+    // if the user is out of guesses or if the park was figured out then restart the game
+    if (Number(localStorage.getItem(localStorageKeys.guessesLeft)) <= 0) {
+        let losses = Number(localStorage.getItem(localStorageKeys.loses)) - 1
+        localStorage.setItem(localStorageKeys.losses, losses)
+    } else if (!hiddenName.includes('_')) {
+        let wins = Number(localStorage.getItem(localStorageKeys.wins)) + 1
+        localStorage.setItem(localStorageKeys.wins, wins)
+    }
+    
+    if (Number(localStorage.getItem(localStorageKeys.guessesLeft)) <= 0 || !hiddenName.includes('_')) {
+        localStorage.setItem(localStorageKeys.lastPark, localStorage.getItem(localStorageKeys.currentPark))
+        localStorage.removeItem(localStorageKeys.currentPark)
+        localStorage.removeItem(localStorageKeys.hiddenName)
+    
+        setupGame(JSON.parse(localStorage.getItem(localStorageKeys.parksLoaded)).start)
+    }
+}
+
+
 setupGame()
-userGuessElement.addEventListener('submit', function (e) {
+userGuessElement.addEventListener('submit', (e) => {
     e.preventDefault()
     e.stopPropagation()
-    const input = userGuessElement.querySelector('input')
-    console.log('this happened: ', input.value.trim())
+
+    // Process what the user entered and clear the field
+    const input = userGuessElement.querySelector('input').value.trim()
+    processUserGuess(input)
+    userGuessElement.querySelector('input').value = ''
 })
